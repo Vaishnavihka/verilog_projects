b41999dda007c0c27a0a8acb68539b5dabeba07b
Vaishnavi A <vaishu3525@gmail.com>
2025-04-06 19:08:09 +0530

    adding test.v file

---
+++ project_1/project_1.srcs/sources_1/new/test.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.04.2025 19:04:22
+// Design Name: 
+// Module Name: test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test(
+
+    );
+endmodule
